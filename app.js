2b8e9839fd6860377f4de78f6f900049b5512671
Himani Mehra <himanimehra528@gmail.com>
2023-08-15 19:02:38 +0530

    interactive comment section

---
+++ app.js
@@ -0,0 +1,231 @@
+const data = {
+    currentUser: {
+      image: {
+        png: "./images/avatars/image-juliusomo.png",
+        webp: "./images/avatars/image-juliusomo.webp",
+      },
+      username: "Jeff Bezos",
+    },
+    comments: [
+      {
+        parent: 0,
+        id: 1,
+        content:
+          "Hello everyone! have developed an interactive comment section , feel free to play with it😉",
+        createdAt: "1 month ago",
+        score: 12,
+        user: {
+          image: {
+            png: "./images/avatars/image-amyrobson.png",
+            webp: "https://media.licdn.com/dms/image/D5603AQHMNPrWxGwIZA/profile-displayphoto-shrink_800_800/0/1691085538187?e=1697673600&v=beta&t=ohGJx1cWrbImAjitpBpfBACUP2qlvkMmwnk87u0zXok",
+          },
+          username: "Himani Mehra",
+        },
+        replies: [],
+      },
+      {
+        parent: 0,
+        id: 2,
+        content:"Oh wow! we can add a comment , can click on '+' , if we wanna add same comments",
+        createdAt: "2 weeks ago",
+        score: 5,
+        user: {
+          image: {
+            png: "./images/avatars/image-maxblagun.png",
+            webp: "./images/avatars/image-maxblagun.webp",
+          },
+          username: "Mark Zuckerberg",
+        },
+        replies: [
+          {
+            parent: 2,
+            id: 1,
+            content:"Woah!! I can even delete a comment , edit it , and can comment upon a comment",
+            createdAt: "1 week ago",
+            score: 4,
+            replyingTo: "Mark Zuckerberg",
+            user: {
+              image: {
+                png: "./images/avatars/image-ramsesmiron.png",
+                webp: "./images/avatars/image-ramsesmiron.webp",
+              },
+              username: "Elon Musk",
+            },
+          },
+          {
+            parent: 2,
+            id: 1,
+            content:"developed using HTML , CSS , Javascript and creativity :p",
+            createdAt: "2 days ago",
+            score: 2,
+            replyingTo: "Elon Musk",
+            user: {
+              image: {
+                png: "./images/avatars/image-juliusomo.png",
+                webp: "./images/avatars/image-juliusomo.webp",
+              },
+              username: "Jeff Bezos",
+            },
+          },
+        ],
+      },
+    ],
+  };
+  function appendFrag(frag, parent) {
+    var children = [].slice.call(frag.childNodes, 0);
+    parent.appendChild(frag);
+    return children[1];
+  }
+  
+  const addComment = (body, parentId, replyTo = undefined) => {
+    let commentParent =
+      parentId === 0
+        ? data.comments
+        : data.comments.filter((c) => c.id == parentId)[0].replies;
+    let newComment = {
+      parent: parentId,
+      id:
+        commentParent.length == 0
+          ? 1
+          : commentParent[commentParent.length - 1].id + 1,
+      content: body,
+      createdAt: "Now",
+      replyingTo: replyTo,
+      score: 0,
+      replies: parent == 0 ? [] : undefined,
+      user: data.currentUser,
+    };
+    commentParent.push(newComment);
+    initComments();
+  };
+  const deleteComment = (commentObject) => {
+    if (commentObject.parent == 0) {
+      data.comments = data.comments.filter((e) => e != commentObject);
+    } else {
+      data.comments.filter((e) => e.id === commentObject.parent)[0].replies =
+        data.comments
+          .filter((e) => e.id === commentObject.parent)[0]
+          .replies.filter((e) => e != commentObject);
+    }
+    initComments();
+  };
+  
+  const promptDel = (commentObject) => {
+    const modalWrp = document.querySelector(".modal-wrp");
+    modalWrp.classList.remove("invisible");
+    modalWrp.querySelector(".yes").addEventListener("click", () => {
+      deleteComment(commentObject);
+      modalWrp.classList.add("invisible");
+    });
+    modalWrp.querySelector(".no").addEventListener("click", () => {
+      modalWrp.classList.add("invisible");
+    });
+  };
+  
+  const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+    if (parent.querySelectorAll(".reply-input")) {
+      parent.querySelectorAll(".reply-input").forEach((e) => {
+        e.remove();
+      });
+    }
+    const inputTemplate = document.querySelector(".reply-input-template");
+    const inputNode = inputTemplate.content.cloneNode(true);
+    const addedInput = appendFrag(inputNode, parent);
+    addedInput.querySelector(".bu-primary").addEventListener("click", () => {
+      let commentBody = addedInput.querySelector(".cmnt-input").value;
+      if (commentBody.length == 0) return;
+      addComment(commentBody, parentId, replyTo);
+    });
+  };
+  
+  const createCommentNode = (commentObject) => {
+    const commentTemplate = document.querySelector(".comment-template");
+    var commentNode = commentTemplate.content.cloneNode(true);
+    commentNode.querySelector(".usr-name").textContent =
+      commentObject.user.username;
+    commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
+    commentNode.querySelector(".score-number").textContent = commentObject.score;
+    commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
+    commentNode.querySelector(".c-body").textContent = commentObject.content;
+    if (commentObject.replyingTo)
+      commentNode.querySelector(".reply-to").textContent =
+        "@" + commentObject.replyingTo;
+  
+    commentNode.querySelector(".score-plus").addEventListener("click", () => {
+      commentObject.score++;
+      initComments();
+    });
+  
+    commentNode.querySelector(".score-minus").addEventListener("click", () => {
+      commentObject.score--;
+      if (commentObject.score < 0) commentObject.score = 0;
+      initComments();
+    });
+    if (commentObject.user.username == data.currentUser.username) {
+      commentNode.querySelector(".comment").classList.add("this-user");
+      commentNode.querySelector(".delete").addEventListener("click", () => {
+        promptDel(commentObject);
+      });
+      commentNode.querySelector(".edit").addEventListener("click", (e) => {
+        console.log(e);
+        const path = e.path[3].querySelector(".c-body");
+        if (
+          path.getAttribute("contenteditable") == false ||
+          path.getAttribute("contenteditable") == null
+        ) {
+          path.setAttribute("contenteditable", true);
+          path.focus()
+        } else {
+          path.removeAttribute("contenteditable");
+        }
+        
+      });
+      return commentNode;
+    }
+    return commentNode;
+  };
+  
+  const appendComment = (parentNode, commentNode, parentId) => {
+    const bu_reply = commentNode.querySelector(".reply");
+    // parentNode.appendChild(commentNode);
+    const appendedCmnt = appendFrag(commentNode, parentNode);
+    const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
+    bu_reply.addEventListener("click", () => {
+      if (parentNode.classList.contains("replies")) {
+        spawnReplyInput(parentNode, parentId, replyTo);
+      } else {
+        //console.log(appendedCmnt.querySelector(".replies"));
+        spawnReplyInput(
+          appendedCmnt.querySelector(".replies"),
+          parentId,
+          replyTo
+        );
+      }
+    });
+  };
+  
+  function initComments(
+    commentList = data.comments,
+    parent = document.querySelector(".comments-wrp")
+  ) {
+    parent.innerHTML = "";
+    commentList.forEach((element) => {
+      var parentId = element.parent == 0 ? element.id : element.parent;
+      const comment_node = createCommentNode(element);
+      if (element.replies && element.replies.length > 0) {
+        initComments(element.replies, comment_node.querySelector(".replies"));
+      }
+      appendComment(parent, comment_node, parentId);
+    });
+  }
+  
+  initComments();
+  const cmntInput = document.querySelector(".reply-input");
+  cmntInput.querySelector(".bu-primary").addEventListener("click", () => {
+    let commentBody = cmntInput.querySelector(".cmnt-input").value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, 0);
+    cmntInput.querySelector(".cmnt-input").value = "";
+  });
+  
+  // addComment("Hello ! It works !!",0);
